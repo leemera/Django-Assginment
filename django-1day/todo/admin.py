dc66f8abeddac260e55c5220fbf3e2cd3241239e
leemera <jseejy57@gmail.com>
2025-05-09 19:53:10 +0900

    ✨ 댓글 기능 생성

--- django-1day/todo/admin.py
+++ django-1day/todo/admin.py
@@ -1,20 +1,20 @@
-from django.contrib import admin
-from .models import Todo
-
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'is_completed', 'start_date', 'end_date')
-    list_filter = ('is_completed',)
-    search_fields = ('title',)
-    ordering = ('start_date',)
-    date_hierarchy = 'start_date'  # ← 요게 날짜 필터
-    fieldsets = (
-        ('Todo Info', {
-            'fields': ('title', 'description', 'is_completed')
-        }),
-        ('Date Range', {
-            'fields': ('start_date', 'end_date')
-        }),
-    )
-
-# Register your models here.
+# from django.contrib import admin
+# from .models import Todo
+#
+# @admin.register(Todo)
+# class TodoAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'description', 'is_completed', 'start_date', 'end_date')
+#     list_filter = ('is_completed',)
+#     search_fields = ('title',)
+#     ordering = ('start_date',)
+#     date_hierarchy = 'start_date'  # ← 요게 날짜 필터
+#     fieldsets = (
+#         ('Todo Info', {
+#             'fields': ('title', 'description', 'is_completed')
+#         }),
+#         ('Date Range', {
+#             'fields': ('start_date', 'end_date')
+#         }),
+#     )
+#
+# # Register your models here.
