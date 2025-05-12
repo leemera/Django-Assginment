06c94f07f664f2979f05b79ea6cdc6d8a9de97e8
leemera <jseejy57@gmail.com>
2025-05-12 15:59:29 +0900

    ✨ 설정 추가하기

--- django-1day/config/settings.py
+++ django-1day/config/settings.py
@@ -30,19 +30,28 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+CUSTOM_APPS = [
     'todo',
     'users',
+]
+
+THIRD_PARTY_APPS = [
     'django_extensions',
-    'accounts',
+    'django_summernote',
+    'django_cleanup',
 
 ]
+
+INSTALLED_APPS = DJANGO_APPS + CUSTOM_APPS + THIRD_PARTY_APPS
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -123,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -131,3 +142,43 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/cbv/todo/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+SUMMERNOTE_CONFIG = {
+ 'iframe': True,
+
+ 'summernote': {
+     'airMode': False,
+     'width': '100%',
+     'height': '480',
+
+
+     'toolbar': [
+['style', ['style']],                      # 스타일 설정
+            ['font', ['bold', 'underline', 'clear']],  # 글꼴 설정: 굵게, 밑줄, 지우기
+            ['color', ['color']],                      # 색상 설정
+            ['para', ['ul', 'ol', 'paragraph']],       # 문단 설정: 글머리 기호, 번호 매기기, 문단
+            ['table', ['table']],                      # 표 삽입
+            ['insert', ['link', 'picture']],           # 삽입 기능: 링크, 그림
+            ['view', ['fullscreen']],                  # 보기 설정: 전체 화면
+        ],
+
+     'lang': 'ko-KR',
+
+'codemirror': {
+            'mode': 'htmlmixed',     # 코드미러의 모드를 htmlmixed로 설정
+            'lineNumbers': 'true',   # 코드미러에서 줄 번호를 표시
+            'theme': 'monokai',      # 코드미러의 테마를 monokai로 설정
+        },
+    },
+
+
+   # 첨부파일 인증 필요 여부 설정
+    'attachment_require_authentication': True,
+
+    # 첨부파일 기능 비활성화 설정
+    'disable_attachment': False,
+
+    # 첨부파일의 절대경로 URI 사용 설정
+    'attachment_absolute_uri': True,
+}
+
