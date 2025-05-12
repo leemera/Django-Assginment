7c83ceb29e07297ebec8d8ea48135e16221f1c93
leemera <jseejy57@gmail.com>
2025-05-12 16:23:58 +0900

    ✨ 모델 수정

--- django-1day/config/settings.py
+++ django-1day/config/settings.py
@@ -42,6 +42,7 @@ DJANGO_APPS = [
 CUSTOM_APPS = [
     'todo',
     'users',
+    'accounts',
 ]
 
 THIRD_PARTY_APPS = [
--- django-1day/todo/models.py
+++ django-1day/todo/models.py
@@ -4,7 +4,9 @@ from django.db import models
 from django.urls import reverse
 
 User = get_user_model()
-
+from PIL import Image
+from pathlib import Path
+from io import BytesIO
 
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
@@ -13,11 +15,49 @@ class Todo(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     is_completed = models.BooleanField(default=False)
+    thumbnail = models.ImageField(
+        upload_to= 'todo/thumbnails/', default='todo/no_image/NO_IMAGE.gif', null=True, blank=True
+        )
+    completed_image = models.ImageField(upload_to='todo/completed/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.completed_image:
+            return super().save(*args, **kwargs)
+
+        image = Image.open(self.completed_image)
+        image.thumbnail((100, 200))
+
+        image_path = Path(self.completed_image)
+
+        thumbnail_name = image_path.stem
+        thumbnail_extension = image_path.suffix
+        thumbnail_filename = f'{thumbnail_name}_thumbnail{thumbnail_extension}'
+
+        if thumbnail_extension in ('.jpg', '.jpeg'):
+            file_type = 'JPEG'
+        elif thumbnail_extension == '.png':
+            file_type = 'PNG'
+        elif thumbnail_extension == '.gif':
+            file_type = 'GIF'
+        else:
+            return super().save(*args, **kwargs)
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, file_type)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumbnail_filename, temp_thumb, save=False)
+
+        temp_thumb.close()
+        return super().save(*args, **kwargs)
+
+
+
     #
     # def get_absolute_url(self):
     #     return reverse('cbv_todo_info', kwargs={'pk': self.pk})
