11428a7c6b65c1a99f68318899d49166120df747
leemera <jseejy57@gmail.com>
2025-05-13 17:25:06 +0900

    ✨ User 모델 UserManger정의

--- django-1day/config/settings.py
+++ django-1day/config/settings.py
@@ -10,17 +10,34 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os.path
+import json
 from pathlib import Path
+
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#secret_config/secret.json 파일 로딩
+SECRET_FILE = os.path.join(BASE_DIR, 'secret_config', 'secret.json')
+try:
+    with open(SECRET_FILE) as f:
+        _secret = json.loads(f)
+except FileNotFoundError:
+    raise ImproperlyConfigured(f"Secret file not found at {SECRET_FILE}")
 
+def get_secret(setting, secret=_secret):
+    try:
+        return secret[setting]
+    except KeyError:
+        raise ImproperlyConfigured(f"Set the {setting} in secret.json file")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hfq$1fra3))@5^%9r6weiu=qv+i_b*edqt4r0)!p=2%8ili6r2'
+SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -184,3 +201,11 @@ SUMMERNOTE_CONFIG = {
     'attachment_absolute_uri': True,
 }
 
+# .secret_config/secret.json
+{
+ "DJANGO_SECRET_KEY": "your secretkey here",
+ "EMAIL": {
+ "USER": "your email here",
+ "PASSWORD": "your email password here"
+  }
+}
